2747719f08a3ac96a4a9b44816c53a2ac523dfb4
Paul Tuck <paul.tuck@oxon.org>
2023-04-18 14:55:14 +0700

    "2023-04-18_14:55+07"

--- web/src/components/Tagline.tsx
+++ web/src/components/Tagline.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-export default function Title() {
+export default function TagLine() {
     return (
         <div className="flex items-center pl-3">
-            <div className="inline-block relative animate-flashing">
-            </div>
+            <div className="inline-block relative animate-flashing"></div>
         </div>
     );
 }
