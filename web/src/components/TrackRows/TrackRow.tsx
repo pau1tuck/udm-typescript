cd729d9037250287082d7986369e5af3eb3b9494
Paul Tuck <paul.tuck@oxon.org>
2023-04-28 13:10:21 +0700

    "2023-04-28_13:10+07"

--- web/src/components/TrackRows/TrackRow.tsx
+++ web/src/components/TrackRows/TrackRow.tsx
@@ -1,4 +1,51 @@
 import React from "react";
+import Image from "next/image";
 import { ITrack } from "@/types/Track.interface";
 import TrackBoxImage from "@/components/TrackBoxes/TrackBoxImage";
 import TrackBoxData from "@/components/TrackBoxes/TrackBoxData";
+
+// Column widths:
+const cw = {
+    number: "20px",
+    image: "56px",
+    track: "1fr",
+    label: "200px",
+    controls: "200px",
+    duration: "20px",
+};
+
+export default function TrackRow() {
+    return (
+        <div className="max-w-1280 mx-auto">
+            <div
+                className="grid h-56 gap-0"
+                style={{
+                    gridTemplateColumns: `${cw.number} ${cw.image} ${cw.track} ${cw.label} ${cw.controls} ${cw.duration}`,
+                }}
+            >
+                <div className="bg-gray-200">#</div>
+                <div className="bg-gray-300"> </div>
+                <div className="bg-gray-400">Title, Artist</div>
+                <div className="bg-gray-500">Label</div>
+                <div className="bg-gray-600">T</div>
+            </div>
+            <div
+                className="grid h-56 gap-0"
+                style={{
+                    gridTemplateColumns: `${cw.number} ${cw.image} ${cw.track} ${cw.label} ${cw.controls} ${cw.duration}`,
+                }}
+            >
+                <div className="bg-gray-200">1</div>
+                <div className="bg-gray-300">
+                    <Image
+                        src="https://via.placeholder.com/56"
+                        alt="Album Cover"
+                    />
+                </div>
+                <div className="bg-gray-400">Song Title, Artist Name</div>
+                <div className="bg-gray-500">Record Label</div>
+                <div className="bg-gray-600">Track Length</div>
+            </div>
+        </div>
+    );
+}
---
+++ web/src/components/TrackRows/TrackRowData.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ITrackData } from "@/types/Track.interface";
+import TrackTitle from "@/components/TrackData/TrackTitle";
+import TrackVersion from "@/components/TrackData/TrackVersion";
+import TrackArtist from "@/components/TrackData/TrackArtist";
+import TrackLabel from "@/components/TrackData/TrackLabel";
+
+export default function TrackRowData(props: { track: ITrackData }) {
+    const { track } = props;
+    return (
+        <div className="mt-3 font-cursive text-center text-white">
+            <div className="mt-2 rounded-md ">
+                <TrackTitle title={track.title} />
+            </div>
+            {track.version && (
+                <div className="mt-2 rounded-md text-tertiary">
+                    (<TrackVersion version={track.version} />)
+                </div>
+            )}
+            <div className="mt-2 rounded-md text-primary">
+                <TrackArtist artist={track.artist} />
+            </div>
+            <div className="mt-2 rounded-md text-secondary">
+                [<TrackLabel label={track.label} />]
+            </div>
+        </div>
+    );
+}
