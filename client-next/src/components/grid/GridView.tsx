8f3d5b086fe392f36d61a716a5be380de5b73b41
Paul Tuck <paul.tuck@oxon.org>
2023-05-24 15:17:14 +0700

    "2023-05-24_15:17+07"

--- client-next/src/components/grid/GridView.tsx
+++ client-next/src/components/grid/GridView.tsx
@@ -1,10 +1,15 @@
 import { data } from "@/dummyData";
 import { TrackBox } from "./TrackBox";
+import { getTrackData } from "@/utils/get-track-data";
 
 export const GridView = () => {
-    const tracks = data;
+    const tracks = getTrackData();
     const trackBox = tracks.map((track, index) => {
         return <TrackBox track={track} key={index} />;
     });
-    return <div className="">{trackBox}</div>;
+    return (
+        <div className="flex min-h-screen flex-col">
+            <div className="flex flex-wrap justify-center">{trackBox}</div>
+        </div>
+    );
 };
