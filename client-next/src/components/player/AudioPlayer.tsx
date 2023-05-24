ba5a59ce48d3b0bad5033b9aac26d3dc7e66c131
Paul Tuck <paul.tuck@oxon.org>
2023-05-24 17:59:57 +0700

    "2023-05-24_17:59+07"

--- client-next/src/components/player/AudioPlayer.tsx
+++ client-next/src/components/player/AudioPlayer.tsx
@@ -22,18 +22,19 @@ export const AudioPlayer = (props: IAudioPlayerProps) => {
     }, [props.nowPlaying]);
 
     return isClient ? (
-        <div style={{ width: "100%", height: "50px" }}>
+        <div style={{ width: "100%", height: "1px" }}>
             <ReactPlayer
                 ref={playerRef}
-                url="YOUR_AUDIO_URL"
+                url={`/media/${props.trackId}`}
                 controls={true}
                 width="100%"
                 height="100%"
                 config={{
                     file: {
                         attributes: {
-                            controlsList: "nodownload", // optional, to prevent downloading
-                            onContextMenu: (e: any) => e.preventDefault(), // optional, to prevent right-click saving
+                            controlsList: "nodownload",
+                            onContextMenu: (e: React.SyntheticEvent) =>
+                                e.preventDefault(),
                         },
                     },
                 }}
